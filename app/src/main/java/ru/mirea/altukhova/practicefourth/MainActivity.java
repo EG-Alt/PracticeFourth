325e0fad6cfb0fe3e5163f5248d7cb48be92622c
EG-Alt <e-altukhova0906@outlook.com>
2021-09-24 16:12:09 +0300

    Initial commit

---
+++ app/src/main/java/ru/mirea/altukhova/practicefourth/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.mirea.altukhova.practicefourth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
